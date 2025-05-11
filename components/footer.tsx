21c7986c9f60ebcaa684549aa26fd22c9cef47e7
Arnav Gupta <arnav090404@Gmail.com>
2025-05-12 00:23:33 +0530

    linkedin added

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,6 +13,9 @@ export const Footer = () => {
                     <Link href='mailto:srishty@varan.tech'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-plus"><path d="M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8" /><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" /><path d="M19 16v6" /><path d="M16 19h6" /></svg>
                     </Link>
+                    <Link href={"https://www.linkedin.com/company/varnan-labs"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+                    </Link>
                 </div>
                 <p>
                     Copyright © 2025 <span className='font-semibold'>varnan</span>
