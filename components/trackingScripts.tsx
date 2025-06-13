8e4c1e7efad306d410f24dbfaca5532be98d9464
Arnav Gupta <arnav090404@Gmail.com>
2025-06-13 18:51:38 +0530

    leadfeeder fix

--- components/trackingScripts.tsx
+++ components/trackingScripts.tsx
@@ -1,17 +1,17 @@
 // components/TrackingScripts.tsx
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function TrackingScripts() {
   useEffect(() => {
     // Google Analytics
-    const gtagScript = document.createElement('script');
-    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-D74NB4RY4M';
+    const gtagScript = document.createElement("script");
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-D74NB4RY4M";
     gtagScript.async = true;
     document.head.appendChild(gtagScript);
 
-    const gtagInit = document.createElement('script');
+    const gtagInit = document.createElement("script");
     gtagInit.innerHTML = `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
@@ -20,26 +20,32 @@ export default function TrackingScripts() {
     `;
     document.head.appendChild(gtagInit);
 
-    // Leadfeeder
-    const leadfeederScript = document.createElement('script');
-    leadfeederScript.innerHTML = `
-      (function(ss,ex){
-        window.ldfdr = window.ldfdr || function(){
-          (ldfdr.q = ldfdr._q || []).push([].slice.call(arguments));
+    (function (ss: string, ex?: string) {
+      (window as any).ldfdr =
+        (window as any).ldfdr ||
+        function (...args: any[]) {
+          ((window as any).ldfdr.q = (window as any).ldfdr.q || []).push(args);
         };
-        (function(d,s){
-          var fs = d.getElementsByTagName(s)[0];
-          function ce(src){
-            var cs = d.createElement(s);
-            cs.src = src;
-            cs.async = 1;
+
+      (function (d: Document, s: string) {
+        const fs = d.getElementsByTagName(s)[0];
+        function ce(src: string) {
+          const cs = d.createElement(s) as HTMLScriptElement;
+          cs.src = src;
+          cs.async = true;
+          if (fs.parentNode) {
             fs.parentNode.insertBefore(cs, fs);
-          };
-          ce('https://sc.lfeeder.com/lftracker_v1' + ss + (ex ? '_' + ex : '') + '.js');
-        })(document,'script');
-      })('lYNOR8xxBxg8WQJZ');
-    `;
-    document.head.appendChild(leadfeederScript);
+          }
+        }
+
+        ce(
+          "https://sc.lfeeder.com/lftracker_v1" +
+            ss +
+            (ex ? "_" + ex : "") +
+            ".js"
+        );
+      })(document, "script");
+    })("lYNOR8xxBxg8WQJZ");
   }, []);
 
   return null;
